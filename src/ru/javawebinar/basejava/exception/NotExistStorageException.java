b33278d46216bd46578bb8ab489d8b75bc57dbc9
RabKvanta <37476810+RabKvanta@users.noreply.github.com>
2020-06-03 00:00:55 +0300

    Lesson_04 add ->exception class - constructor(message, uuid)

--- src/ru/javawebinar/basejava/exception/NotExistStorageException.java
+++ src/ru/javawebinar/basejava/exception/NotExistStorageException.java
@@ -2,6 +2,6 @@ package ru.javawebinar.basejava.exception;
 
 public class NotExistStorageException extends StorageException {
     public NotExistStorageException(String uuid) {
-        super(uuid);
+        super("Resume " + uuid + " not exist", uuid);
     }
 }
--- src/ru/javawebinar/basejava/exception/StorageException.java
+++ src/ru/javawebinar/basejava/exception/StorageException.java
@@ -3,7 +3,8 @@ package ru.javawebinar.basejava.exception;
 public class StorageException extends RuntimeException {
     private final String uuid;
 
-    public StorageException(String uuid) {
+    public StorageException(String message, String uuid) {
+        super(message);
         this.uuid = uuid;
     }
 }
