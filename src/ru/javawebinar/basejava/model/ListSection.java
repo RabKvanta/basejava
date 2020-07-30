a763f62ea773e1d7e3ff2fadd50c9c8aed3a3d6f
RabKvanta <37476810+RabKvanta@users.noreply.github.com>
2020-07-31 01:39:30 +0300

    Lesson_07: TextSection & ListSection

---
+++ src/ru/javawebinar/basejava/model/ListSection.java
@@ -0,0 +1,9 @@
+package ru.javawebinar.basejava.model;
+
+import java.util.List;
+
+public class ListSection {
+
+    private List<String> list;
+
+}
---
+++ src/ru/javawebinar/basejava/model/TextSection.java
@@ -0,0 +1,7 @@
+package ru.javawebinar.basejava.model;
+
+public class TextSection {
+
+    private String content;
+
+}
