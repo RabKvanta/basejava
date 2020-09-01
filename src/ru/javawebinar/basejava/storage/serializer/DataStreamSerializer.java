a7d424738f373bf3a03aeda396b54982ac77dc83
RabKvanta <37476810+RabKvanta@users.noreply.github.com>
2020-09-01 18:10:34 +0300

    HW10: refactoring with Lamda; added Function Interfaces: FuncRead, FuncWrite, FuncSection; added Methods writeWithException, readWithException,  addElementResumeWithExceptin;

--- src/ru/javawebinar/basejava/storage/serializer/DataStreamSerializer.java
+++ src/ru/javawebinar/basejava/storage/serializer/DataStreamSerializer.java
@@ -5,25 +5,26 @@ import ru.javawebinar.basejava.model.*;
 import java.io.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
 public class DataStreamSerializer implements StreamSerializer {
+
     @Override
     public void doWrite(Resume r, OutputStream os) throws IOException {
         try (DataOutputStream dos = new DataOutputStream(os)) {
             dos.writeUTF(r.getUuid());
             dos.writeUTF(r.getFullName());
             Map<ContactType, String> contacts = r.getContacts();
-            dos.writeInt(contacts.size());
-            for (Map.Entry<ContactType, String> entry : contacts.entrySet()) {
+            writeWithException(contacts.entrySet(), dos, entry -> {
                 dos.writeUTF(entry.getKey().name());
                 dos.writeUTF(entry.getValue());
-            }
+            });
+
             // Implements sections
             Map<SectionType, AbstractSection> sections = r.getSections();
-            dos.writeInt(sections.size());
-            for (Map.Entry<SectionType, AbstractSection> entry : sections.entrySet()) {
+            writeWithException(sections.entrySet(), dos, entry -> {
                 SectionType type = entry.getKey();
                 dos.writeUTF(type.name());
                 switch (type) {
@@ -31,34 +32,30 @@ public class DataStreamSerializer implements StreamSerializer {
                     case PERSONAL:
                         dos.writeUTF(entry.getValue().toString());
                         break;
+
                     case ACHIEVEMENT:
                     case QUALIFICATION:
-                        List<String> listStrings = ((ListSection) entry.getValue()).getItems();
-                        dos.writeInt(listStrings.size());
-                        for (String s : listStrings) {
-                            dos.writeUTF(s);
-                        }
+                        writeWithException(((ListSection) entry.getValue()).getItems(), dos, s -> dos.writeUTF(s));
                         break;
+
                     case EXPERIENCE:
                     case EDUCATION:
-                        List<Experience> experiences = ((ExperienceSection) entry.getValue()).getExperiences();
-                        dos.writeInt(experiences.size());
-                        for (Experience ex : experiences) {
+                        writeWithException(((ExperienceSection) entry.getValue()).getExperiences(), dos, ex -> {
                             dos.writeUTF(ex.getHomePage().getName());
                             dos.writeUTF(ex.getHomePage().getUrl());
-                            List<Experience.Position> positions = ex.getPositions();
-                            dos.writeInt(positions.size());
 
-                            for (Experience.Position pos : positions) {
+                            writeWithException(ex.getPositions(), dos, pos -> {
                                 dos.writeUTF(pos.getStartDate().toString());
                                 dos.writeUTF(pos.getEndDate().toString());
                                 dos.writeUTF(pos.getTitle());
                                 dos.writeUTF(pos.getDescription());
-                            }
-                        }
+                            });
+                        });
+
                         break;
                 }
-            }
+            });
+
         }
     }
 
@@ -68,53 +65,76 @@ public class DataStreamSerializer implements StreamSerializer {
             String uuid = dis.readUTF();
             String fullName = dis.readUTF();
             Resume resume = new Resume(uuid, fullName);
-            int size = dis.readInt();
-            for (int i = 0; i < size; i++) {
-                resume.addContact(ContactType.valueOf(dis.readUTF()), dis.readUTF());
-            }
 
+            addElementResumeWithException(dis, () -> resume.addContact(ContactType.valueOf(dis.readUTF()), dis.readUTF()));
             // Implements sections
-            size = dis.readInt();
-            for (int i = 0; i < size; i++) {
+            addElementResumeWithException(dis, () -> {
                 SectionType sectionType = SectionType.valueOf(dis.readUTF());
-                switch (sectionType) {
-                    case OBJECTIVE:
-                    case PERSONAL:
-                        resume.addSection(sectionType, new TextSection(dis.readUTF()));
-                        break;
-                    case ACHIEVEMENT:
-                    case QUALIFICATION:
-                        List<String> listStrings = new ArrayList<>();
-                        int sizeList = dis.readInt();
-                        for (int k = 0; k < sizeList; k++) {
-                            listStrings.add(dis.readUTF());
-                        }
-                        resume.addSection(sectionType, new ListSection(listStrings));
-                        break;
-                    case EXPERIENCE:
-                    case EDUCATION:
-                        List<Experience> experiences = new ArrayList<>();
-                        int sizeEx = dis.readInt();
-                        for (int k = 0; k < sizeEx; k++) {
-                            String linkName = dis.readUTF();
-                            String linkUrl = dis.readUTF();
-                            int sizePos = dis.readInt();
-                            List<Experience.Position> positions = new ArrayList<>();
-                            for (int j = 0; j < sizePos; j++) {
-                                positions.add(new Experience.Position(
-                                        LocalDate.parse(dis.readUTF()),
-                                        LocalDate.parse(dis.readUTF()),
-                                        dis.readUTF(), dis.readUTF()));
-                            }
-                            experiences.add(new Experience(new Link(linkName, linkUrl), positions));
-                        }
-                        resume.addSection(sectionType, new ExperienceSection(experiences));
-                        break;
-
-                }
-            }
+                resume.addSection(sectionType, readSection(dis, sectionType));
+            });
 
             return resume;
         }
+    }
+
+    private <T> void writeWithException(Collection<T> collection, DataOutputStream dos, FuncWrite<T> funcWrite) throws IOException {
+        dos.writeInt(collection.size());
+        for (T t : collection) {
+            funcWrite.write(t);
+        }
+    }
+
+    private <T> List<T> readWithException(DataInputStream dis, FuncRead<T> funcRead) throws IOException {
+        int size = dis.readInt();
+        List<T> list = new ArrayList<>();
+        for (int k = 0; k < size; k++) {
+            list.add(funcRead.read());
+        }
+        return list;
+    }
+
+    private void addElementResumeWithException(DataInputStream dis, FuncSection func) throws IOException {
+        int size = dis.readInt();
+        for (int k = 0; k < size; k++) {
+            func.addElement();
+        }
+
+    }
+
+    private interface FuncSection {
+        void addElement() throws IOException;
+    }
+
+    private interface FuncWrite<T> {
+        void write(T t) throws IOException;
+    }
+
+    private interface FuncRead<T> {
+        T read() throws IOException;
+    }
+
+
+    private AbstractSection readSection(DataInputStream dis, SectionType sectionType) throws IOException {
+
+        switch (sectionType) {
+            case OBJECTIVE:
+            case PERSONAL:
+                return new TextSection(dis.readUTF());
+
+            case ACHIEVEMENT:
+            case QUALIFICATION:
+                return new ListSection(readWithException(dis, () -> dis.readUTF()));
+
+            case EXPERIENCE:
+            case EDUCATION:
+                return (new ExperienceSection(readWithException(dis, () -> new Experience(new Link(dis.readUTF(), dis.readUTF()),
+                        readWithException(dis, () -> new Experience.Position(
+                                LocalDate.parse(dis.readUTF()),
+                                LocalDate.parse(dis.readUTF()),
+                                dis.readUTF(), dis.readUTF()))))));
+            default:
+                return null;
+        }
+
     }
 }
